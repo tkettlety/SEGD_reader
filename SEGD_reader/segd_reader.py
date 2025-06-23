7d62bbc57f193a1567dc7733133b8e5d77e15888
sachalapins <sacha.lapins@gmail.com>
2025-06-23 13:59:31 +0100

    Updated header comments

--- SEGD_reader/segd_reader.py
+++ SEGD_reader/segd_reader.py
@@ -17,7 +17,7 @@ Includes
 
 MIT License
 
-Copyright (c) 2024 Sacha Lapins
+Copyright (c) 2025 Sacha Lapins
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
